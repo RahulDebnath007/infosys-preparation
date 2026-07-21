8a18f102e2bc388af0658cb83c9412007d36d899
Rahul Debnath <rahuldebnath7914@gmail.com>
2026-07-22 01:45:18 +0530

    2nd question

--- count_arrays.py
+++ count_arrays.py
@@ -1,13 +1,25 @@
-MOD=10000
-def countArrays(n,k):
-    dp = [[0]*(n+1) for _ in range(k+1)]
-    for num in range(1,n+1):
-        dp[1][num] = 1
-    for length in range(1,k):
-        for num in range(1,n+1):
-           for nultiple in range(num,n+1,num):
-                dp[length+1][multiple]= (dp[length+1][multiple]+ dp[length][num]) % MOD
+MOD = 10000
 
+def countArrays(n, k):
+
+    # dp[length][number]
+    dp = [[0] * (n + 1) for _ in range(k + 1)]
+
+    # Base case
+    for num in range(1, n + 1):
+        dp[1][num] = 1
+
+    # Build DP
+    for length in range(1, k):
+        for num in range(1, n + 1):
+
+            # Visit every multiple of num
+            for multiple in range(num, n + 1, num):
+                dp[length + 1][multiple] = (
+                    dp[length + 1][multiple] + dp[length][num]
+                ) % MOD
+
+    # Sum all arrays of length k
     return sum(dp[k]) % MOD
 
 
@@ -15,4 +27,4 @@ def countArrays(n,k):
 n = int(input())
 k = int(input())
 
-print(countArrays(n, k))            
+print(countArrays(n, k))
