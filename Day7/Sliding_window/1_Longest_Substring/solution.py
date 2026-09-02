d52e5f593dd963e74c244f6aa546c66617b8100d
Rahul Debnath <rahuldebnath7914@gmail.com>
2026-09-02 21:43:45 +0530

    add sliding window

---
+++ Day7/Sliding_window/1_Longest_Substring/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+
+        seen = {}
+        start = 0
+        max_len = 0
+
+        for end, char in enumerate(s):
+
+            if char in seen and seen[char] >= start:
+                start = seen[char] + 1
+
+            seen[char] = end
+
+            current_len = end - start + 1
+
+            if current_len > max_len:
+                max_len = current_len
+
+        return max_len
